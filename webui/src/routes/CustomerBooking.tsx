4c54c75d7d87ac75218b57c4c8e014d32f907c0c
3DRX <kjy20030228@icloud.com>
2024-04-20 02:38:59 +0800

    Implementing booking form
    Finished everything except calling the API.

--- webui/src/routes/CustomerBooking.tsx
+++ webui/src/routes/CustomerBooking.tsx
@@ -2,17 +2,11 @@ import { DatePickerWithRange } from "@/components/DatePickerWithRange";
 import { NavBar } from "@/components/NavBar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { addDays } from "date-fns";
 import { useForm } from "react-hook-form";
+import { DateRange } from "shared";
 import { z } from "zod";
 
 const bookingFormSchema = z.object({
@@ -20,22 +14,45 @@ const bookingFormSchema = z.object({
     from: z.date(),
     to: z.date(),
   }),
-  email: z.string().email(),
 });
 
 export const CustomerBooking = () => {
+  const disabledDays: Date[] = [
+    new Date(),
+    addDays(new Date(), 1),
+    new Date(2024, 4, 3),
+  ];
+  // TODO: fetch disabledDays from server
+
+  function findFirstDisabledDay(from: Date, to: Date): Date | undefined {
+    for (let i = 0; i < disabledDays.length; i++) {
+      const disabledDay = disabledDays[i];
+      if (disabledDay > from && disabledDay <= to) {
+        return disabledDay;
+      }
+    }
+    return undefined;
+  }
+
   const form = useForm<z.infer<typeof bookingFormSchema>>({
     resolver: zodResolver(bookingFormSchema),
     defaultValues: {
       date: {
-        from: new Date(),
-        to: addDays(new Date(), 1),
+        // from: new Date(),
+        // to: addDays(new Date(), 1),
+        from: undefined,
+        to: undefined,
       },
     },
   });
 
   function onSubmit(values: z.infer<typeof bookingFormSchema>) {
-    console.log(JSON.stringify(values));
+    const dateRange = DateRange.parse({
+      startDate: values.date.from,
+      endDate: values.date.to,
+    });
+    console.log(JSON.stringify(dateRange));
+    // TODO: post dateRange to server
   }
 
   return (
@@ -59,7 +76,44 @@ export const CustomerBooking = () => {
                     <FormControl>
                       <DatePickerWithRange
                         date={field.value}
-                        setDate={field.onChange}
+                        setDate={(newVal) => {
+                          // @ts-ignore
+                          if (newVal.to === undefined) {
+                            field.onChange(newVal);
+                          } else {
+                            // @ts-ignore
+                            const firstDisabledDay = findFirstDisabledDay(
+                              // @ts-ignore
+                              newVal.from,
+                              // @ts-ignore
+                              newVal.to,
+                            );
+                            if (firstDisabledDay === undefined) {
+                              field.onChange(newVal);
+                            } else {
+                              // @ts-ignore
+                              if (newVal.from === field.value.from) {
+                                field.onChange({
+                                  // @ts-ignore
+                                  from: newVal.to,
+                                  to: undefined,
+                                });
+                              } else {
+                                field.onChange({
+                                  // @ts-ignore
+                                  from: newVal.from,
+                                  to: undefined,
+                                });
+                              }
+                            }
+                          }
+                        }}
+                        disabledDays={[
+                          {
+                            before: new Date(),
+                          },
+                          ...disabledDays,
+                        ]}
                         className="w-[300px]"
                       />
                     </FormControl>
@@ -70,23 +124,6 @@ export const CustomerBooking = () => {
                     ) : (
                       <></>
                     )}
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        type="email"
-                        placeholder="邮箱"
-                        className="w-[300px]"
-                      />
-                    </FormControl>
-                    <FormMessage />
                   </FormItem>
                 )}
               />
