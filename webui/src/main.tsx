54d28dc7bd065473e51acacf5e8ba8f2b45281b6
3DRX <kjy20030228@icloud.com>
2024-04-22 09:35:47 +0800

    Add shadcnui tasks example in airconmanager route.

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -12,6 +12,7 @@ import { Register } from "./routes/Register.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ProtectedRoute } from "./components/ProtectedRoute.tsx";
 import { AuthProvider } from "./components/AuthProvider.tsx";
+import { AirconDashboard } from "./routes/AirconDashboard.tsx";
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,10 @@ const router = createBrowserRouter([
         </ProtectedRoute>
       </AuthProvider>
     ),
+  },
+  {
+    path: "airconmanager",
+    element: <AirconDashboard />,
   },
   {
     path: "register",
