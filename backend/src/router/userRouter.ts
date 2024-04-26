4f109f1748f6bff6a18f2dc4510b07cd05130e73
Ruining Chen <nerlci.underline@outlook.com>
2024-04-26 13:36:12 +0800

    add logout for user management (#22)

--- backend/src/router/userRouter.ts
+++ backend/src/router/userRouter.ts
@@ -7,4 +7,6 @@ userRouter.post("/register", userController.registerUser);
 
 userRouter.post("/login", userController.loginUser);
 
+userRouter.get("/logout", userController.logoutUser);
+
 export { userRouter };
