c198ba08baa5e29ed058a9a6a34207615f99b8fc
3DRX <kjy20030228@icloud.com>
2024-04-28 20:44:50 +0800

    Add: mode switch, tempEmulate, BYPASS_AUTH env var.

--- webui/src/components/AirconDrawer.tsx
+++ webui/src/components/AirconDrawer.tsx
@@ -8,7 +8,6 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { useState } from "react";
 import { Minus, Plus } from "lucide-react";
 import {
   MAX_AIRCON_SPEED,
@@ -18,42 +17,53 @@ import {
 } from "shared";
 import AirConditionerIcon from "../assets/aircon.svg";
 import { Switch } from "./ui/switch";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useSSE } from "@/hooks/useSse";
-import { BASE_URL } from "@/lib/dataFetch";
-import { useEffect } from "react";
+// import { Skeleton } from "@/components/ui/skeleton";
+// import { useSSE } from "@/hooks/useSse";
+// import { BASE_URL } from "@/lib/dataFetch";
+// import { useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import { useTempEmulate } from "@/lib/tempEmulate";
 
 export function AirconDrawer() {
-  const [tempreture, settempreture] = useState(24);
-  const [windspeed, setwindspeed] = useState(1);
-  const [start, setstart] = useState(false);
+  const [temperature, setTemperature] = useLocalStorage("temperature", 24);
+  const [windspeed, setwindspeed] = useLocalStorage("windspeed", 1);
+  const [start, setstart] = useLocalStorage("start", false);
+  const [cool, setcool] = useLocalStorage("cool", true);
+  const currentTemp = useTempEmulate({
+    startTemp: 10,
+    targetTemp: temperature,
+    windSpeed: windspeed,
+    mode: cool ? "cool" : "heat",
+    start: start,
+  });
 
-  const { sseData, sseReadyState, closeSource } = useSSE<{
-    data: string;
-  }>(`${BASE_URL}/api/ac/status`);
+  // const { sseData, sseReadyState, closeSource } = useSSE<{
+  //   data: string;
+  // }>(`${BASE_URL}/api/ac/status`);
 
-  useEffect(() => closeSource, [closeSource]);
+  // useEffect(() => closeSource, [closeSource]);
 
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <Button className="h-14 w-40" variant="outline">
-          <div className="flex flex-initial flex-row gap-2">
+        <Button className="w-70 h-14" variant="outline">
+          <div className="flex flex-initial flex-row gap-5">
             <img
               className="pointer-events-none w-10 select-none invert-0 dark:invert"
               src={AirConditionerIcon}
             />
-            {start && (
-              <>
-                <p>{tempreture}&deg;C</p>
-                <p>{windspeed}</p>
-              </>
-            )}
-            {sseReadyState.key === 0 ? (
-              <Skeleton className="h-5 w-20" />
-            ) : (
-              <div>{sseData && sseData.data}</div>
-            )}
+            <div
+              className={`flex flex-row flex-wrap items-center justify-center gap-1 ${start ? "" : "text-zinc-700"}`}
+            >
+              <p className="w-28">目标温度：{temperature}&deg;C</p>
+              <p className="w-24">目标风速：{windspeed}</p>
+              <p className="w-28">当前温度：{currentTemp}&deg;C</p>
+              {/*sseReadyState.key === 0 ? (
+                <Skeleton className="h-5 w-20" />
+              ) : (
+                <div>{sseData && sseData.data}</div>
+              )*/}
+            </div>
           </div>
         </Button>
       </DrawerTrigger>
@@ -78,19 +88,32 @@ export function AirconDrawer() {
           </DrawerHeader>
           <div className="p-4 pb-0">
             <div className="flex items-center justify-center space-x-2">
+              <div className="flex w-full justify-start gap-4">
+                <Switch
+                  className="ml-2 data-[state=checked]:bg-blue-500 data-[state=unchecked]:bg-orange-500"
+                  checked={cool}
+                  onCheckedChange={setcool}
+                  disabled={!start}
+                />
+                <div className={`${start ? "" : "text-zinc-700"}`}>
+                  {cool ? "制冷" : "制热"}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-center space-x-2">
               <Button
                 variant="outline"
                 size="icon"
                 className="h-8 w-8 shrink-0 rounded-full"
                 onClick={() => {
-                  settempreture((prev) =>
+                  setTemperature((prev) =>
                     Math.max(
                       MIN_AIRCON_TEMP,
                       Math.min(MAX_AIRCON_TEMP, prev - 1),
                     ),
                   );
                 }}
-                disabled={tempreture <= MIN_AIRCON_TEMP || !start}
+                disabled={temperature <= MIN_AIRCON_TEMP || !start}
               >
                 <Minus className="h-4 w-4" />
                 <span className="sr-only">Decrease</span>
@@ -99,7 +122,7 @@ export function AirconDrawer() {
                 <div
                   className={`flex items-center justify-center gap-1 text-7xl font-bold tracking-tighter ${start ? "" : "text-muted"}`}
                 >
-                  {tempreture}
+                  {temperature}
                   <div className="text-4xl">&deg;C</div>
                 </div>
                 <div
@@ -113,14 +136,14 @@ export function AirconDrawer() {
                 size="icon"
                 className="h-8 w-8 shrink-0 rounded-full"
                 onClick={() => {
-                  settempreture((prev) =>
+                  setTemperature((prev) =>
                     Math.max(
                       MIN_AIRCON_TEMP,
                       Math.min(MAX_AIRCON_TEMP, prev + 1),
                     ),
                   );
                 }}
-                disabled={tempreture >= MAX_AIRCON_TEMP || !start}
+                disabled={temperature >= MAX_AIRCON_TEMP || !start}
               >
                 <Plus className="h-4 w-4" />
                 <span className="sr-only">Increase</span>
