8e508f4a9b72cf3442d1b4549b24a6396f834f01
Nerlci_ <nerlci.underline@outlook.com>
2024-04-29 10:12:12 +0800

    implement statusService for managing SseChannel

--- backend/src/service/statusService.ts
+++ backend/src/service/statusService.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { ACStatus } from 'shared';
 const SseChannel = require('sse-channel');
 
-const channels = new Map();
+const channels: Map<string, any> = new Map();
 
 const getChannel = (roomId: string) => {
     // TODO: Check if the room is valid
@@ -16,10 +16,11 @@ const getChannel = (roomId: string) => {
 const listenStatus = async (req: Request, res: Response, roomId: string) => {
     const channel = getChannel(roomId);
     channel.addClient(req, res);
+    // TODO: Send initial status
 }
 
-const updateStatus = async (roomId: string, status: ACStatus) => {
-    const channel = getChannel(roomId);
+const updateStatus = async (status: ACStatus) => {
+    const channel = getChannel(status.roomId);
     channel.send(JSON.stringify(status));
 }
 
