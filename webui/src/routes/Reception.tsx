dcb633217a38d27637a5688a90533d0be0998a16
jingyang kang <kjy20030228@icloud.com>
2024-04-30 21:29:47 +0800

    Add reception route.

---
+++ webui/src/routes/Reception.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from "@/components/NavBar";
+import { PersonIcon } from "@radix-ui/react-icons";
+
+export default function Reception() {
+  return (
+    <>
+      <NavBar title={<PersonIcon />} />
+    </>
+  );
+}
