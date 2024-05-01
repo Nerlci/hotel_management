1c27736d82bb96f453992053cfcdfe81b66b0095
Nerlci_ <nerlci.underline@outlook.com>
2024-05-01 13:10:46 +0800

    fix several bugs in scheduler logic and simplify code

--- backend/src/service/schedulerService.ts
+++ backend/src/service/schedulerService.ts
@@ -12,7 +12,23 @@ const ROUND_ROBIN_INTERVAL = Number(process.env.ROUND_ROBIN_INTERVAL);
 
 const waitingList: SchedulerItem[] = [];
 const servingList: SchedulerItem[] = [];
-const roundRobinList: { interval: NodeJS.Timeout, item: SchedulerItem }[] = [];
+const rrList: { interval: NodeJS.Timeout, item: SchedulerItem }[] = [];
+
+const modifyTimestamps = (item: ACUpdateRequest, now: Date) => {
+  return {
+    ...item,
+    onTimestamp: now,
+    timestamp: now,
+  };
+}
+
+const stopRR = (roomId: string) => {
+  const idx = rrList.findIndex(item => item.item.roomId === roomId);
+  if (idx === -1) { return; }
+
+  clearInterval(rrList[idx].interval);
+  rrList.splice(idx, 1);
+}
 
 const statusChange = async (status: SchedulerItem) => {
   const { userId, onTimestamp, ...rest } = status;
@@ -36,78 +52,59 @@ const statusChange = async (status: SchedulerItem) => {
 const checkWaitingList = () => {
   if (waitingList.length === 0) { return; }
 
-  const sortedList = servingList.sort((a, b) => 
+  waitingList.sort((a, b) => 
     a.fanSpeed === b.fanSpeed ? b.fanSpeed - a.fanSpeed: b.onTimestamp.getTime() - a.onTimestamp.getTime()
   );
-  waitingList.splice(0, waitingList.length);
-  for (const waitingItem of sortedList) {
+  const minServingFanSpeed = Math.min(...servingList.map(item => item.fanSpeed));
+  for (const waitingItem of waitingList) {
+    if (waitingItem.fanSpeed < minServingFanSpeed && servingList.length >= SERVE_LIMIT) { continue; }
     schedulerStep(waitingItem);
   }
 }
 
 const preemptService = (item: SchedulerItem, preemptedItem: SchedulerItem) => {
-  const now = new Date();
-  const idx = servingList.findIndex(item => item.roomId === preemptedItem.roomId);
-  servingList.splice(idx, 1);
+  const now = item.timestamp;
+  const servingIdx = servingList.findIndex(item => item.roomId === preemptedItem.roomId);
+  servingList.splice(servingIdx, 1);
+
+  const waitingIdx = waitingList.findIndex(waitingItem => waitingItem.roomId === item.roomId);
+  if (waitingIdx !== -1) {
+    waitingList.splice(waitingIdx, 1);
+  }
 
   statusChange(item);
-  servingList.push({
-    ...item,
-    onTimestamp: now,
-    timestamp: now,
-  });
+  servingList.push(item);
 
   statusChange({
     ...preemptedItem,
     on: false,
   });
-  waitingList.push({
-    ...preemptedItem,
-    onTimestamp: now,
-    timestamp: now,
-  });
+  waitingList.push(modifyTimestamps(preemptedItem, now));
 
-  const rrIdx = roundRobinList.findIndex(rrItem => rrItem.item.roomId === item.roomId);
+  const rrIdx = rrList.findIndex(rrItem => rrItem.item.roomId === item.roomId);
   if (rrIdx !== -1) {
-    clearInterval(roundRobinList[rrIdx].interval);
-    roundRobinList.splice(rrIdx, 1);
+    clearInterval(rrList[rrIdx].interval);
+    rrList.splice(rrIdx, 1);
+  }
 
+  if (rrIdx !== -1 && item.fanSpeed === preemptedItem.fanSpeed) {
     const newInterval = setInterval(rrPreempt, ROUND_ROBIN_INTERVAL, preemptedItem.roomId);
-    roundRobinList.push({
+    rrList.push({
       interval: newInterval,
-      item: {
-        ...preemptedItem,
-        onTimestamp: now,
-        timestamp: now,
-      }
+      item: modifyTimestamps(preemptedItem, now),
     });
   }
 }
 
 const rrPreempt = (roomId: string) => {
-  const idx = roundRobinList.findIndex(item => item.item.roomId === roomId);
+  const idx = rrList.findIndex(item => item.item.roomId === roomId);
   if (idx === -1) { return; }
 
-  if (servingList.length < SERVE_LIMIT) {
-    const item = roundRobinList[idx].item;
-    const now = new Date();
-    statusChange(item);
-    servingList.push({
-      ...item,
-      onTimestamp: now,
-      timestamp: now,
-    });
-    clearInterval(roundRobinList[idx].interval);
-    roundRobinList.splice(idx, 1);
-
-    return;
-  }
-
-  const item = roundRobinList[idx].item;
-  const interval = roundRobinList[idx].interval;
+  const item = rrList[idx].item;
+  const interval = rrList[idx].interval;
   const sameFanSpeedItems = servingList.filter(servingItem => servingItem.fanSpeed === item.fanSpeed).sort((a, b) => a.onTimestamp.getTime() - b.onTimestamp.getTime());
   if (sameFanSpeedItems.length === 0) {
-    roundRobinList.splice(idx, 1);
+    rrList.splice(idx, 1);
     clearInterval(interval);
     return;
   }
@@ -116,15 +113,8 @@ const rrPreempt = (roomId: string) => {
   preemptService(item, preemptedItem);
 }
 
-const schedulerStep = (item: ACUpdateRequest) => {
+const schedulerStep = (item: SchedulerItem) => {
   const now = new Date();
-
-  // 如果请求对象在时间片调度队列中，将其移除
-  const rrIdx = roundRobinList.findIndex(rrItem => rrItem.item.roomId === item.roomId);
-  if (rrIdx !== -1) {
-    clearInterval(roundRobinList[rrIdx].interval);
-    roundRobinList.splice(rrIdx, 1);
-  }
 
   // 如果正在服务
   const servingItemIdx = servingList.findIndex(servingItem => servingItem.roomId === item.roomId);
@@ -138,34 +128,38 @@ const schedulerStep = (item: ACUpdateRequest) => {
 
     if (!item.on) {
       servingList.splice(servingItemIdx, 1);
-      
       checkWaitingList();
-    } else {
-      servingList[servingItemIdx] = modifiedItem;
-      if (item.fanSpeed < servingList[servingItemIdx].fanSpeed) {
-        checkWaitingList();
-      }
+
+      return;
+    }
+
+    const previousFanSpeed = servingList[servingItemIdx].fanSpeed;
+    servingList[servingItemIdx] = modifiedItem;
+    if (item.fanSpeed < previousFanSpeed) {
+      checkWaitingList();
     }
 
     return;
   }
 
-  // 如果正在等待
+  // 如果正在等待，先将其移除
   const waitingItemIdx = waitingList.findIndex(waitingItem => waitingItem.roomId === item.roomId);
   if (waitingItemIdx !== -1) {
     waitingList.splice(waitingItemIdx, 1);
   }
 
-  // 如果没有正在服务，且请求关闭服务，则直接返回
-  if (!item.on) { return; }
+  // 如果没有正在服务，且请求关闭服务
+  if (!item.on) { 
+    // 如果请求关闭服务的对象在时间片等待中，将其移除
+    stopRR(item.roomId);
+
+    return; 
+  }
 
   // 当服务对象数小于服务对象上限时，所有请求会被分配一个服务对象
   if (servingList.length < SERVE_LIMIT) {
-    const modifiedItem = {
-      ...item,
-      onTimestamp: now,
-      timestamp: now,
-    };
+    stopRR(item.roomId);
+    const modifiedItem = modifyTimestamps(item, now);
     statusChange(modifiedItem);
     servingList.push(modifiedItem);
 
@@ -186,12 +180,7 @@ const schedulerStep = (item: ACUpdateRequest) => {
     // 如果只有一个风速最低的服务对象，或者有多个但风速不相等
     if (minFanSpeedItems.length === 1 || new Set(minFanSpeedItems.map(item => item.fanSpeed)).size > 1) {
       const minFanSpeedItem = minFanSpeedItems[0];
-      const modifiedItem = {
-        ...item,
-        onTimestamp: now,
-        timestamp: now,
-      };
-      preemptService(modifiedItem, minFanSpeedItem);
+      preemptService(modifyTimestamps(item, now), minFanSpeedItem);
 
       return;
     }
@@ -199,44 +188,29 @@ const schedulerStep = (item: ACUpdateRequest) => {
     // 如果有多个风速相等且低于请求风速的服务对象，找到服务时长最大的服务对象
     const maxServiceDuration = Math.max(...minFanSpeedItems.map(item => now.getTime() - item.onTimestamp.getTime()));
     const maxServiceDurationItem = minFanSpeedItems.find(item => now.getTime() - item.onTimestamp.getTime() === maxServiceDuration)!;
-    const modifiedItem = {
-      ...item,
-      onTimestamp: now,
-      timestamp: now,
-    };
-    preemptService(modifiedItem, maxServiceDurationItem);
+    preemptService(modifyTimestamps(item, now), maxServiceDurationItem);
+
+    return;
   }
 
   // 如果请求风速等于某些服务对象的风速，启动时间片调度策略
   if (requestFanSpeed === Math.min(...servingFanSpeeds)) {
-    // 将请求对象放置于等待队列，并分配一个等待服务时长
-    // 在这两分钟期间，如果没有任何服务状态的变化，当等待服务时长=0 时将服务队列中服务时长最大的服务对象释放
-    // 在这等待的两分钟期间内，如果有任何一个服务对象的目标温度到达或关机，则等待队列中的等待服务时长最小的对象获得服务对象
+    // 将请求对象放置于等待队列，设置时间片
+    const rrIdx = rrList.findIndex(rrItem => rrItem.item.roomId === item.roomId);
+    if (rrIdx !== -1) { return; }
     
-    const roundRobinInterval = setInterval(rrPreempt, ROUND_ROBIN_INTERVAL, item.roomId);
-    roundRobinList.push({
-      interval: roundRobinInterval,
-      item: {
-        ...item,
-        onTimestamp: now,
-        timestamp: now,
-      },
+    const rrInterval = setInterval(rrPreempt, ROUND_ROBIN_INTERVAL, item.roomId);
+    rrList.push({
+      interval: rrInterval,
+      item: modifyTimestamps(item, now),
     });
-    waitingList.push({
-      ...item,
-      onTimestamp: now,
-      timestamp: now,
-    });
+    waitingList.push(modifyTimestamps(item, now));
 
     return;
   }
 
   // 如果请求风速小于所有服务对象的风速，请求对象必须等到某个服务对象空闲后才能得到服务
-  waitingList.push({
-    ...item,
-    onTimestamp: now,
-    timestamp: now,
-  });
+  waitingList.push(item);
   
   return;
 }
@@ -259,7 +233,7 @@ const addUpdateRequest = async (request: ACUpdateRequest) => {
     data: data,
   });
 
-  schedulerStep(request);
+  schedulerStep(modifyTimestamps(request, now));
 }
 
 const schedulerService = {
