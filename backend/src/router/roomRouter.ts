25725337685a014eccaa165948bd847a3aa29028
weiguang1 <weiguangwzh@qq.com>
2024-05-03 00:01:22 +0800

    Add routers for user room management.

---
+++ backend/src/router/roomRouter.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import {roomController} from "../controller/roomController";
+import {authUserMiddleware} from "../controller/userController";
+
+let roomRouter = express.Router();
+
+roomRouter.post("/book", authUserMiddleware, roomController.bookRoom);
+roomRouter.get("/order", authUserMiddleware, roomController.queryRoom);
+roomRouter.delete("/book", authUserMiddleware, roomController.cancelOrder);
+export {roomRouter};
