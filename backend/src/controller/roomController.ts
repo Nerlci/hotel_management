28d536e0f4766eea07cf5ad74af1f7dfa1950c24
Zhenhu Wu <95153598+weiguang1@users.noreply.github.com>
2024-05-03 16:40:38 +0800

    Room service on the user side. (#39)
    * Add tables for room management.
    
    * Add routers for room management (userside).
    
    * Add user functions for room management.
    
    * Add routers for user room management.
    
    * Add functions to check room availability.
    
    * Modify the return value of queryRoom to be string.
    
    * Add unique constraint to userId in table reservation.
    
    * Remove mock for room availability...
    
    * Add unique constraint for booking.
    
    * Add room table initialization.
    
    * Debug checkDaysAvailability.
    
    * Add router for availability check.
    
    * Change the logic of room availability check.

---
+++ backend/src/controller/roomController.ts
@@ -0,0 +1,163 @@
+import {Request, Response} from "express";
+import {acUpdateRequest, responseBase} from "shared";
+import {roomService} from "../service/roomService";
+import {prisma} from "../prisma";
+
+const bookRoom = async (req: Request, res: Response) => {
+	const startDate = new Date(req.body.startDate);
+	const endDate = new Date(req.body.endDate);
+	const userId = res.locals.user.userId;
+
+	// 一个用户只能订一间房
+	const userRoom = await prisma.reservation.findMany({
+		where: {
+			userId: userId,
+		},
+	});
+
+	if (userRoom.length > 0) {
+		const response = responseBase.parse({
+			error: {
+				msg: "You have already booked a room",
+			},
+			code: "400",
+			payload: {},
+		});
+
+		res.json(response);
+		return;
+	}
+
+	if (!(await roomService.checkRoomAvailability(startDate, endDate))) {
+		const response = responseBase.parse({
+			error: {
+				msg: "No room available. Please choose another date.",
+			},
+			code: "400",
+			payload: {},
+		});
+
+		res.json(response);
+	} else {
+		await prisma.reservation.create({
+			data: {
+				userId: userId,
+				startDate: startDate,
+				endDate: endDate,
+			},
+		});
+
+		const response = responseBase.parse({
+			error: {
+				msg: "",
+			},
+			code: "200",
+			payload: {},
+		});
+
+		res.json(response);
+	}
+};
+
+// 订单查询
+const queryRoom = async (req: Request, res: Response) => {
+	const userId = res.locals.user.userId;
+	const room = await prisma.reservation.findMany({
+		where: {
+			userId: userId,
+		},
+	});
+
+	if (room.length === 0) {
+		const response = responseBase.parse({
+			error: {
+				msg: "No reservation",
+			},
+			code: "400",
+			payload: {},
+		});
+
+		res.json(response);
+		return;
+	} else {
+		const response = responseBase.parse({
+			error: {
+				msg: "",
+			},
+			code: "200",
+			payload: {
+				roomId: room[0].roomId ? room[0].roomId : "",
+			},
+		});
+
+		res.json(response);
+	}
+};
+
+const checkDaysAvailability = async (req: Request, res: Response) => {
+	const startDate = new Date(req.query.startDate as string);
+	const endDate = new Date(req.query.endDate as string);
+
+	const busyDays = await roomService.findBusyDays(startDate, endDate);
+
+	const response = responseBase.parse({
+		error: {
+			msg: "",
+		},
+		code: "200",
+		payload: {
+			unavailableDates: busyDays,
+		},
+	});
+
+	res.json(response);
+};
+
+// 客房退订
+const cancelOrder = async (req: Request, res: Response) => {
+	const userId = res.locals.user.userId;
+
+	const room = await prisma.reservation.findMany({
+		where: {
+			userId: userId,
+		},
+	});
+
+	if (room.length === 0) {
+		const response = responseBase.parse({
+			error: {
+				msg: "No reservation",
+			},
+			code: "400",
+			payload: {},
+		});
+
+		res.json(response);
+		return;
+	} else {
+		await prisma.reservation.delete({
+			where: {
+				userId: userId,
+			},
+		});
+
+		const response = responseBase.parse({
+			error: {
+				msg: "",
+			},
+			code: "200",
+			payload: {},
+		});
+
+		res.json(response);
+	}
+};
+
+const roomController = {
+	bookRoom,
+	queryRoom,
+	checkDaysAvailability,
+	cancelOrder,
+};
+
+export {roomController};
---
+++ backend/src/service/roomService.ts
@@ -0,0 +1,57 @@
+import {prisma} from "../prisma";
+const totalRooms = parseInt(process.env.TOTAL_ROOMS || "2");
+
+async function checkRoomAvailability(
+	checkInDate: Date,
+	checkOutDate: Date
+): Promise<boolean> {
+	// 查询与给定日期冲突的预定数
+	const countConflictingReservations = await prisma.reservation.count({
+		where: {
+			startDate: {
+				lte: checkOutDate,
+			},
+			endDate: {
+				gte: checkInDate,
+			},
+		},
+	});
+
+	// 如果冲突的预定数小于房间总数，则有空房
+	return countConflictingReservations < totalRooms;
+}
+
+async function findBusyDays(startDate: Date, endDate: Date) {
+	var busyDays = [];
+	var currentDate = new Date(startDate);
+	while (currentDate <= endDate) {
+		if (!(await checkRoomAvailability(currentDate, currentDate))) {
+			busyDays.push(currentDate.toISOString().split("T")[0]);
+		}
+		currentDate.setDate(currentDate.getDate() + 1);
+	}
+
+	return busyDays;
+}
+
+// 房间初始化
+const initRoom = async () => {
+	const rooms = await prisma.room.findMany();
+	if (rooms.length === 0) {
+		for (let i = 0; i < 1; i++) {
+			await prisma.room.create({
+				data: {
+					roomId: `100${i}`,
+				},
+			});
+		}
+	}
+};
+
+const roomService = {
+	findBusyDays,
+	checkRoomAvailability,
+};
+
+export {roomService};
+export {initRoom};
