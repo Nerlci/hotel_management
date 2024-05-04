ee957dfa5fbd93d7ad7f7ccaf021205e49e56517
Jingyang Kang <kjy20030228@icloud.com>
2024-05-04 21:35:37 +0800

    Native UI routes (#49)
    * Fix broken expo router project setup
    
    * Cleans up example project, add basic layouts.

--- nativeui/app/(tabs)/index.tsx
+++ nativeui/app/(tabs)/index.tsx
@@ -1,12 +1,65 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { Button } from "@/components/Button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/Card";
 import { Text, View } from "@/components/Themed";
 
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">Tab One</Text>
+      <Text className="text-3xl font-bold">预定</Text>
       <View className="my-6 h-[1px] w-80 bg-gray-200" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View className="w-4/5">
+        <Card>
+          <CardHeader>
+            <CardTitle>填写日期</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <View className="ml-0 mr-auto mt-auto flex flex-row items-center justify-center gap-2">
+              <View className="flex flex-row items-center justify-center">
+                <Text className="text-lg">从</Text>
+                <View className="w-24">
+                  <RNDateTimePicker
+                    mode="date"
+                    value={new Date()}
+                    timeZoneName={"Asia/Shanghai"}
+                    locale="zh-CN"
+                  />
+                </View>
+              </View>
+              <View className="flex flex-row items-center justify-center">
+                <Text className="text-lg">到</Text>
+                <View className="w-24">
+                  <RNDateTimePicker
+                    mode="date"
+                    value={new Date()}
+                    timeZoneName={"Asia/Shanghai"}
+                    locale="zh-CN"
+                  />
+                </View>
+              </View>
+            </View>
+          </CardContent>
+          <CardFooter>
+            <View className="flex flex-row">
+              <View className="grow" />
+              <Button
+                onPress={() => {
+                  console.log("确定");
+                }}
+              >
+                确定
+              </Button>
+              <View className="grow" />
+            </View>
+          </CardFooter>
+        </Card>
+      </View>
     </View>
   );
 }
