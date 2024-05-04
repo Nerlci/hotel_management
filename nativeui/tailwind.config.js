84ac046a7424666c7ead1a138e3e7933f107b6a5
3DRX <kjy20030228@icloud.com>
2024-05-04 17:34:38 +0800

    Add pre-commit hook for nativeui, add NativeCn ui.

--- nativeui/tailwind.config.js
+++ nativeui/tailwind.config.js
@@ -1,8 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
