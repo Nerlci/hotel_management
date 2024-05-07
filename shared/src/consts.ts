16c60bb68c18a719d12d23907a597859549ca258
3DRX <kjy20030228@icloud.com>
2024-05-07 10:30:28 +0800

    Update consts

--- shared/src/consts.ts
+++ shared/src/consts.ts
@@ -1,6 +1,8 @@
 export const NAME = "廉价酒店管理系统";
 
-export const MAX_AIRCON_TEMP = 30;
+export const MAX_AIRCON_TEMP = 28;
 export const MIN_AIRCON_TEMP = 18;
-export const MAX_AIRCON_SPEED = 5;
+export const DEFAULT_AIRCON_TEMP = 25;
+export const MAX_AIRCON_SPEED = 3;
 export const MIN_AIRCON_SPEED = 1;
+export const DEFAULT_AIRCON_SPEED = 2;
