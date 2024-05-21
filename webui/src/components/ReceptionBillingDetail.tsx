851ca71dab5fa99e5fcbc1b3fb21bbbf512ba816
Jingyang Kang <kjy20030228@icloud.com>
2024-05-21 10:18:32 +0800

    update reception ui (#81)
    Co-authored-by: Davian <daixinwang@bupt.edu.cn>

--- webui/src/components/ReceptionBillingDetail.tsx
+++ webui/src/components/ReceptionBillingDetail.tsx
@@ -28,7 +28,7 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
           <CardTitle className="group flex items-center gap-2 text-lg">
             {roomId ? (
               <>
-                {`Order ${roomId}`}
+                {`${roomId} 房间详单`}
                 <Button
                   size="icon"
                   variant="outline"
@@ -43,19 +43,17 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
             )}
           </CardTitle>
           {roomId ? (
-            <CardDescription>November 23, 2023</CardDescription>
+            <CardDescription className="flex justify-between gap-4">
+              <span>2024-05-15 11:05</span>
+              <span>账单号:000293</span>
+              <span>结账员:王丽</span>
+            </CardDescription>
           ) : (
             <Skeleton className="mb-1 mt-1 h-3 w-20" />
           )}
         </div>
         {roomId ? (
           <div className="ml-auto flex items-center gap-1">
-            <Button size="sm" variant="outline" className="h-8 gap-1">
-              <Truck className="h-3.5 w-3.5" />
-              <span className="sr-only xl:not-sr-only xl:whitespace-nowrap">
-                Track Order
-              </span>
-            </Button>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button size="icon" variant="outline" className="h-8 w-8">
@@ -64,10 +62,10 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
-                <DropdownMenuItem>Edit</DropdownMenuItem>
-                <DropdownMenuItem>Export</DropdownMenuItem>
+                <DropdownMenuItem>编辑</DropdownMenuItem>
+                <DropdownMenuItem>导出</DropdownMenuItem>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>Trash</DropdownMenuItem>
+                <DropdownMenuItem>删除</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
@@ -77,20 +75,14 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
       </CardHeader>
       <CardContent className="p-6 text-sm">
         <div className="grid gap-3">
-          <div className="font-semibold">Order Details</div>
+          <div className="font-semibold">住宿费明细</div>
           {roomId ? (
             <ul className="grid gap-3">
               <li className="flex items-center justify-between">
                 <span className="text-muted-foreground">
-                  Glimmer Lamps x <span>2</span>
+                  标准间 x <span>1</span>
                 </span>
-                <span>$250.00</span>
-              </li>
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">
-                  Aqua Filters x <span>1</span>
-                </span>
-                <span>$49.00</span>
+                <span>￥250.00</span>
               </li>
             </ul>
           ) : (
@@ -99,24 +91,17 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
               <Skeleton className="h-5 w-52" />
             </>
           )}
-          <Separator className="my-2" />
+          <Separator className="my-4" />
+          <div className="font-semibold">餐饮费明细</div>
           {roomId ? (
             <ul className="grid gap-3">
               <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Subtotal</span>
-                <span>$299.00</span>
+                <span className="text-muted-foreground">健康轻食早餐 x 1</span>
+                <span>￥20.00</span>
               </li>
               <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Shipping</span>
-                <span>$5.00</span>
-              </li>
-              <li className="flex items-center justify-between">
-                <span className="text-muted-foreground">Tax</span>
-                <span>$25.00</span>
-              </li>
-              <li className="flex items-center justify-between font-semibold">
-                <span className="text-muted-foreground">Total</span>
-                <span>$329.00</span>
+                <span className="text-muted-foreground">家庭式晚餐套餐 x 1</span>
+                <span>￥140.00</span>
               </li>
             </ul>
           ) : (
@@ -130,39 +115,63 @@ export default function ReceptionBillingDetail({ roomId }: DetailProps) {
         </div>
         <Separator className="my-4" />
         <div className="grid gap-3">
-          <div className="font-semibold">顾客信息</div>
+          <div className="font-semibold">空调费明细</div>
           {roomId ? (
-            <dl className="grid gap-3">
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Customer</dt>
-                <dd>Liam Johnson</dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Email</dt>
-                <dd>
-                  <a href="mailto:">liam@acme.com</a>
-                </dd>
-              </div>
-              <div className="flex items-center justify-between">
-                <dt className="text-muted-foreground">Phone</dt>
-                <dd>
-                  <a href="tel:">+1 234 567 890</a>
-                </dd>
-              </div>
-            </dl>
+            <ul className="grid gap-3">
+              <li className="flex items-center justify-between font-semibold">
+                <span className="text-muted-foreground">
+                  0:00 - 7:00 设定温度:24°C 风速:低
+                </span>
+                <span>￥3.00</span>
+              </li>
+              <li className="flex items-center justify-between font-semibold">
+                <span className="text-muted-foreground">
+                  10:00 - 12:00 设定温度:24°C 风速:中
+                </span>
+                <span>￥1.20</span>
+              </li>
+            </ul>
           ) : (
             <>
-              <Skeleton className="h-4 w-36" />
-              <Skeleton className="h-4 w-28" />
-              <Skeleton className="mb-3 h-4 w-24" />
+              <Skeleton className="h-5 w-36" />
+              <Skeleton className="h-5 w-28" />
             </>
           )}
         </div>
+        <Separator className="my-4" />
+        <div className="grid gap-3">
+          {roomId ? (
+            <ul className="grid gap-3">
+              <li className="flex items-center justify-between font-semibold">
+                <span className="text-muted-foreground">总费用</span>
+                <span>￥414.20</span>
+              </li>
+            </ul>
+          ) : (
+            <>
+              <Skeleton className="h-5 w-36" />
+              <Skeleton className="h-5 w-28" />
+              <Skeleton className="h-5 w-24" />
+              <Skeleton className="h-5 w-32" />
+            </>
+          )}
+        </div>
+        <Separator className="my-4" />
+        <ul className="grid gap-3">
+          <li className="flex items-center justify-between">
+            <span className="text-muted-foreground">地址</span>
+            <span>北京市朝阳区朝阳路 37 号</span>
+          </li>
+          <li className="flex items-center justify-between">
+            <span className="text-muted-foreground">电话</span>
+            <span>010-12345678</span>
+          </li>
+        </ul>
       </CardContent>
       <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
         {roomId ? (
           <div className="ml-auto text-xs text-muted-foreground">
-            更新日期 <time dateTime="2024-69-42">2024 年 69 月 42 日</time>
+            巴普特酒店
           </div>
         ) : (
           <Skeleton className="ml-auto h-4 w-36" />
