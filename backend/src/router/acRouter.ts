7e58e8196eae83a7a995e0a9dc6772a20a02ea6f
Ruining Chen <nerlci.underline@outlook.com>
2024-05-22 15:41:05 +0800

    statement-file patch (#94)
    * implement target and price rate apis
    
    * statement-file patch

--- backend/src/router/acRouter.ts
+++ backend/src/router/acRouter.ts
@@ -15,9 +15,25 @@ acRouter.get("/temp", authUserMiddleware, acController.tempAC);
 acRouter.get("/statement", authUserMiddleware, acController.statementAC);
 
 acRouter.get(
-  "/statement-table",
+  "/statement-file",
   authUserMiddleware,
-  acController.statementTableAC,
+  acController.statementFileAC,
+);
+
+acRouter.get("/price-rate", authUserMiddleware, acController.getPriceRateAC);
+
+acRouter.put("/price-rate", authUserMiddleware, acController.setPriceRateAC);
+
+acRouter.get(
+  "/target-range",
+  authUserMiddleware,
+  acController.getTargetRangeAC,
+);
+
+acRouter.put(
+  "/target-range",
+  authUserMiddleware,
+  acController.setTargetRangeAC,
 );
 
 acRouter.get("/price-rate", authUserMiddleware, acController.getPriceRateAC);
