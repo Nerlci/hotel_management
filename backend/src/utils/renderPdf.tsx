bcf5c91893a0ea43fb3b1d292f2eec5736576966
3DRX <3drxkjy@gmail.com>
2024-05-27 21:14:51 +0800

    Tweak statements document style.

--- backend/src/utils/renderPdf.tsx
+++ backend/src/utils/renderPdf.tsx
@@ -2,8 +2,8 @@ import ReactPDF, {
   Page,
   Text,
   Document,
-  StyleSheet,
   Font,
+  View,
 } from "@react-pdf/renderer";
 import { Table, TR, TH, TD } from "@ag-media/react-pdf-table";
 import { StatementItem } from "shared";
@@ -23,87 +23,174 @@ Font.register({
   src: "./src/utils/SourceHanSerifSC-VF.ttf",
 });
 
-const styles = StyleSheet.create({
-  page: {
-    fontFamily: "HanSerifSC-VF",
-    flexDirection: "column",
-    backgroundColor: "#fff",
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-});
-
 // 详单
 const Statement = (props: {
   headers: string[];
   statements: (string | number | null)[][];
   roomId: string;
 }) => {
+  const sum = props.statements.reduce((acc, cur) => {
+    return acc + Number(cur[5]);
+  }, 0);
+
   return (
     <Document>
-      <Page size="A4" style={styles.page} orientation="landscape">
-        <Text
+      <Page
+        size="A4"
+        style={{
+          fontFamily: "HanSerifSC-VF",
+          flexDirection: "column",
+          backgroundColor: "#fff",
+          paddingTop: 10,
+          paddingBottom: 10,
+          paddingLeft: 10,
+          paddingRight: 10,
+        }}
+      >
+        <View
           style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginBottom: 5,
+            display: "flex",
+            flexDirection: "row",
+            marginVertical: 15,
+            marginHorizontal: 10,
+            alignItems: "flex-end",
           }}
         >
-          {props.roomId} 详单
-        </Text>
-        <Table
-          style={{
-            fontSize: 12,
-          }}
-        >
-          <TH
+          <Text
             style={{
-              backgroundColor: "#ddd",
+              fontSize: 40,
             }}
           >
-            {props.headers.map((header) => (
-              <TD key={header}>
-                <Text
-                  style={{
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    fontSize: 16,
-                  }}
-                >
-                  {header}
-                </Text>
-              </TD>
-            ))}
-          </TH>
-          {props.statements.map((statement, index) => (
-            <TR
-              key={statement[1]}
+            HOTEL-BUPT
+          </Text>
+          <View style={{ flexGrow: 1 }} />
+          <Text
+            style={{
+              fontSize: 16,
+            }}
+          >
+            {props.roomId} 详单
+          </Text>
+        </View>
+        <View
+          style={{
+            marginHorizontal: 10,
+          }}
+        >
+          <Table
+            style={{
+              fontSize: 12,
+            }}
+          >
+            <TH
               style={{
-                backgroundColor: !isEven(index) ? "#eee" : "#fff",
+                backgroundColor: "#BED0F5",
+                height: 25,
               }}
             >
-              {Object.values(statement).map((value) => (
-                <TD key={value}>
+              {props.headers.map((header, index) => (
+                <TD
+                  key={header}
+                  style={{
+                    border: "none",
+                  }}
+                >
                   <Text
                     style={{
                       marginLeft: "auto",
                       marginRight: "auto",
+                      fontSize: 12,
                     }}
                   >
-                    {value}
+                    {header}
                   </Text>
                 </TD>
               ))}
-            </TR>
-          ))}
-        </Table>
+            </TH>
+            {props.statements.map((statement, index) => (
+              <TR
+                key={statement[1]}
+                style={{
+                  backgroundColor: !isEven(index) ? "#E0EEFF" : "#fff",
+                }}
+              >
+                {statement.map((value, index) => (
+                  <TD
+                    key={value}
+                    style={{
+                      border: "none",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        fontSize: index < 3 ? 8 : 10,
+                      }}
+                    >
+                      {value}
+                      {index === 5 || index === 6
+                        ? "￥"
+                        : index === 7 || index === 8
+                          ? "°C"
+                          : index === 3
+                            ? "s"
+                            : ""}
+                    </Text>
+                  </TD>
+                ))}
+              </TR>
+            ))}
+          </Table>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginVertical: 10,
+            marginHorizontal: 10,
+          }}
+        >
+          <View style={{ flexGrow: 1 }} />
+          <Text
+            style={{
+              fontSize: 12,
+            }}
+          >
+            总计：{sum.toFixed(2)}￥
+          </Text>
+        </View>
+        <View style={{ flexGrow: 1 }} />
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            fontSize: 10,
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <View style={{ flexGrow: 1 }} />
+            <Text>{new Date().toLocaleDateString("zh-CN")}</Text>
+          </View>
+          <View style={{ flexGrow: 1 }} />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Text>巴普特酒店</Text>
+            <Text>地址：北京市朝阳区朝阳路 37 号</Text>
+            <Text>电话：010-12345678</Text>
+          </View>
+        </View>
       </Page>
     </Document>
   );
